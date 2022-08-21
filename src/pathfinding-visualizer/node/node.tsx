a3482ab2805287d6465b2dc6f037328a179896e6
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-20 20:08:25 -0400

    configured ts

--- src/pathfinding-visualizer/node/node.tsx
+++ src/pathfinding-visualizer/node/node.tsx
@@ -1,11 +1,12 @@
 import "./node.css";
+import { CurrentNode } from "../pathfinding-vizualizer";
+
+type Node = {
+  node: CurrentNode;
+};
 
 const Node = () => {
-  return (
-    <div className="node">
-
-    </div>
-  );
+  return <div className="node"></div>;
 };
 
 export default Node;
--- src/pathfinding-visualizer/pathfinding-vizualizer.tsx
+++ src/pathfinding-visualizer/pathfinding-vizualizer.tsx
@@ -1,27 +1,45 @@
 import { useEffect, useState } from "react";
 import Node from "./node/node";
 
+export type CurrentNode = {
+  col: number;
+  row: number;
+  isStart?: boolean;
+  isFinish?: boolean;
+};
+
 const PathfindingVisualizer = () => {
-  const [nodes, setNodes] = useState<[][][][]>([]);
+  const [nodes, setNodes] = useState<CurrentNode[][]>([]);
 
   useEffect(() => {
     const nodes = [];
-    for (let row=0; row<20; row++) {
+    for (let row = 0; row < 20; row++) {
       const currentRow = [];
-      for (let col=0; col<50; col++) {
-        currentRow.push([]);
-      };
+      for (let col = 0; col < 50; col++) {
+        const currentNode: CurrentNode = {
+          col,
+          row,
+          isStart: col === 10 && row === 5,
+          isFinish: col === 10 && row === 45,
+        };
+
+        currentRow.push(currentNode);
+      }
       nodes.push(currentRow);
       setNodes(nodes);
-    };
+    }
   }, []);
 
   return (
     <div className="grid">
       {nodes.map((row, rowIdx) => {
-        return <div key={rowIdx}>
-          {row.map((node, nodeIdx) => <Node key={nodeIdx}/>)}
-        </div>
+        return (
+          <div key={rowIdx}>
+            {row.map((node, nodeIdx) => (
+              <Node key={nodeIdx} />
+            ))}
+          </div>
+        );
       })}
     </div>
   );
