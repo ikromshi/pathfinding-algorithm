59e7e354cb53a518de540487de58902d14475241
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-24 20:21:52 -0400

    refactored the pathfinding-visualizer component

--- src/algorithms/dijkstra.ts
+++ src/algorithms/dijkstra.ts
@@ -5,15 +5,12 @@ export type Node = {
   distance: number;
 };
 
-// const node: Node = {
-//   row, col, isVisited, distance
-// };
-
-const dijkstra = (nodes: Node[], startNode: number, finishNode: number) => {
+const dijkstra = (grid: Node[], startNode: {}, finishNode: {}) => {
   if (!startNode || !finishNode || startNode === finishNode) return false;
   
-  nodes[startNode].distance = 0;
-  const unvisitedNodes = nodes.slice();
+  // Every node's distance - except the start node - is infinity initially
+  grid[startNode].distance = 0;
+  const unvisitedNodes = grid.slice();
   while(!!unvisitedNodes.length) {
     sortNodesByDistance(unvisitedNodes);
     const closestNode = unvisitedNodes.shift();
@@ -29,20 +26,42 @@ const dijkstra = (nodes: Node[], startNode: number, finishNode: number) => {
      * 
      * 
      * 
+     * 
+     * 
+     * 
+     * Handle the animation of dijkstraVisualize here>>>
+     * grid[object] -> number?
+     * 
      */
+
     if (closestNode) {
       closestNode.isVisited = true;
     };
 
-    // BUG!!!! Is node a number of Obj????
+    // Come back to this abomination later pls
     if (closestNode === finishNode) return "success";
-    /**
-     * Update neighbors here
-     */
+    closestNode && updateNeighbors(closestNode, grid);
   };
 };
 
-// Sort nodes in ascending order depending on their distance;
+// Sort grid in ascending order depending on their distance;
 const sortNodesByDistance = (unvisitedNodes: Node[]) => {
   unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+};
+
+function updateNeighbors(node: Node, grid:Node[]) {
+  const neighbors = getNeighbors(node, grid);
+  for (const neighbor of neighbors) {
+    neighbor.distance = node.distance + 1;
+  };
+};
+
+function getNeighbors(node: Node, table: Node[]) {
+  const neighbors = [];
+  const { col, row } = node;
+  if (row > 0) neighbors.push(table[row-1][col]);
+  if (row < table.length-1) neighbors.push(table[row+1][col]);
+  if (col > 0) neighbors.push(table[row][col-1]);
+  if (col < table[0].length-1) neighbors.push(table[row][col+1]);
+  return neighbors;
 };
--- src/pathfinding-visualizer/pathfinding-vizualizer.tsx
+++ src/pathfinding-visualizer/pathfinding-vizualizer.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
+import { Fragment } from "react";
 import Node from "./node/node";
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
 
 export type CurrentNode = {
   col: number;
@@ -9,43 +15,65 @@ export type CurrentNode = {
 };
 
 const PathfindingVisualizer = () => {
-  const [nodes, setNodes] = useState<CurrentNode[][]>([]);
+  const [grid, setGrid] = useState<CurrentNode[][]>([]);
 
+  /**
+   * Construct the initial grid and pass it down to the Node component ~
+   * in the return block to build each individual cell;
+   */
   useEffect(() => {
-    const nodes = [];
-    for (let row = 0; row < 20; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        const currentNode: CurrentNode = {
-          col,
-          row,
-          isStart: row === 10 && col === 5,
-          isFinish: row === 10 && col === 45,
-        };
-
-        currentRow.push(currentNode);
-      }
-      nodes.push(currentRow);
-      setNodes(nodes);
-    }
+    const grid = getInitialGrid();
+    setGrid(grid);
   }, []);
 
+  function visualizeDijkstra() {}
+
   return (
-    <div className="grid">
-      {nodes.map((row, rowIdx) => {
-        return (
-          <div key={rowIdx}>
-            {row.map((node, nodeIdx) => {
-              const { isStart, isFinish } = node;
-              return (
-                <Node key={nodeIdx} isStart={isStart} isFinish={isFinish} />
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+    <Fragment>
+      <button onClick={() => visualizeDijkstra()}>
+        Visualize Dijkstra's Algorithm
+      </button>
+      <div className="grid">
+        {grid.map((row, rowIdx) => {
+          return (
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node key={nodeIdx} isStart={isStart} isFinish={isFinish} />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </Fragment>
   );
 };
+
+function getInitialGrid() {
+  const grid: CurrentNode[][] = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow: CurrentNode[] = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(col, row));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+}
+
+function createNode(col: number, row: number) {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col == FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: false,
+  };
+}
 
 export default PathfindingVisualizer;
